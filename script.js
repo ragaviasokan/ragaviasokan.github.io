27b107a8c91c50ff50b25a64e78ee2b73609bce1
ragaviasokan <rasokan@torontomu.ca>
2024-03-11 22:48:49 -0400

    up

--- script.js
+++ script.js
@@ -29,11 +29,11 @@ function resetColorOnOut(element) {
 }
 
 //main.js
-/* When the user clicks on the button,toggle between hiding and showing the dropdown content */
+/* clicks on the button,toggle between hiding and showing the dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
     }
-    // Close the dropdown menu if the user clicks outside of it
+    // Close the dropdown menu 
     window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");

d32ad1c70ff523336abdc6281562228488091e3a
ragaviasokan <rasokan@torontomu.ca>
2024-03-25 17:25:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -63,3 +63,33 @@ function clearForm() {
     // Clear all input fields
     document.getElementById("user-form").reset();
 }
+
+//accessibility
+  // Function to change font size
+  document.getElementById('fontSize').addEventListener('change', function() {
+    var fontSize = this.value;
+    document.body.style.fontSize = fontSize;
+  });
+
+  // Function to change color scheme
+  document.getElementById('colorScheme').addEventListener('change', function() {
+    var colorScheme = this.value;
+    switch(colorScheme) {
+      case 'normal':
+        document.body.style.backgroundColor = '#ecf8fa'; // blue background
+        document.body.style.color = '#000000'; // Black text
+        break;
+      case 'protanopia':
+        document.body.style.backgroundColor = '#f9f2dc'; // Cream background
+        document.body.style.color = '#000000'; // Black text
+        break;
+      case 'deuteranopia':
+        document.body.style.backgroundColor = '#e0f0d4'; // Pale green background
+        document.body.style.color = '#000000'; // Black text
+        break;
+      case 'tritanopia':
+        document.body.style.backgroundColor = '#fff8fa'; // Pale pink background
+        document.body.style.color = '#000000'; // Black text
+        break;
+    }
+  });
